7ec648536fe02e94155871bfeb2145b1a134b80c
Joe Zachary <zachary@cs.utah.edu>
2016-03-25 22:08:00 -0600

    Added starting point for BoggleService implementation

---
+++ BoggleService/BoggleService/IBoggleService.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace Boggle
+{
+    [ServiceContract]
+    public interface IBoggleService
+    {
+        /// <summary>
+        /// Sends back index.html as the response body.
+        /// </summary>
+        [WebGet(UriTemplate = "/api")]
+        Stream API();
+
+        /// <summary>
+        /// Demo.  You can delete this.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        [WebGet(UriTemplate = "/numbers?length={n}")]
+        IList<int> Numbers(string n);
+
+        /// <summary>
+        /// Demo.  You can delete this.
+        /// </summary>
+        [WebInvoke(Method = "POST", UriTemplate = "/first")]
+        int GetFirst(IList<int> list);
+    }
+}
