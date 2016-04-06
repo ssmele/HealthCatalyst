c22f7bd213fefcada0a200dce5add488e9c40cf9
ssmele <ssmele1995@hotmail.com>
2016-04-06 14:05:11 -0600

    Changed comman.

--- BoggleService/BoggleService/BoggleService.svc.cs
+++ BoggleService/BoggleService/BoggleService.svc.cs
@@ -278,11 +278,11 @@ namespace Boggle
                     string Player1UserToken = "", Player2UserToken = "";
 
                     //Determine which state we are in. 
-                    using (SqlCommand GameInfoCommand = new SqlCommand("Select * from Games where GameID = @GameID", conn, trans))
+                    using (SqlCommand command = new SqlCommand("Select * from Games where GameID = @GameID", conn, trans))
                     {
-                        GameInfoCommand.Parameters.AddWithValue("@GameID", GivenGameID);
+                        command.Parameters.AddWithValue("@GameID", GivenGameID);
 
-                        using (SqlDataReader reader = GameInfoCommand.ExecuteReader())
+                        using (SqlDataReader reader = command.ExecuteReader())
                         {
                             //If there was no GameID by the given GameID
                             if (!reader.HasRows)
@@ -323,19 +323,21 @@ namespace Boggle
                                     returnInfo.GameState = "active";
                                     returnInfo.TimeLeft = TimeLimit-minusTime;
                                 }
+
                             }
                             reader.Close();
                         }
                     }
+                    
 
                     //Getting scores for both players. 
-                    using (SqlCommand ScoreCommand = new SqlCommand("Select Sum(Score) as Player1Score from Words where GameID = @GameID and Player = @Player1UserToken union Select Sum(Score) as Player2Score from Words where GameID = @GameID and Player = @Player2UserToken ", conn, trans))
+                    using (SqlCommand command = new SqlCommand("Select Sum(Score) as Player1Score from Words where GameID = @GameID and Player = @Player1UserToken union Select Sum(Score) as Player2Score from Words where GameID = @GameID and Player = @Player2UserToken ", conn, trans))
                     {
-                        ScoreCommand.Parameters.AddWithValue("@Player1UserToken", Player1UserToken);
-                        ScoreCommand.Parameters.AddWithValue("@Player2UserToken", Player2UserToken);
-                        ScoreCommand.Parameters.AddWithValue("@GameID", GameID);
+                        command.Parameters.AddWithValue("@Player1UserToken", Player1UserToken);
+                        command.Parameters.AddWithValue("@Player2UserToken", Player2UserToken);
+                        command.Parameters.AddWithValue("@GameID", GameID);
 
-                        using (SqlDataReader reader2 = ScoreCommand.ExecuteReader())
+                        using (SqlDataReader reader2 = command.ExecuteReader())
                         {
                             reader2.Read();
                             object Score2 = reader2["Score"];
